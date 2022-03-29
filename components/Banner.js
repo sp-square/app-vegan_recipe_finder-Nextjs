2d7c31effeab5ddb52662b23ad66e9da14a0d175
Sandrine Poissonnet <spoisson@gmail.com>
2022-03-29 10:06:58 -0700

    Banner basic

---
+++ components/Banner.js
@@ -0,0 +1,18 @@
+import styles from './Banner.module.css';
+
+const Banner = (props) => {
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.title}>
+				<span className={styles.title1}>Assiette</span>
+				<span className={styles.title2}>Verte</span>
+			</h1>
+			<p className={styles.subtitle}>Discover plant-based recipes!</p>
+			<button className={styles.button} onClick={props.handleOnClick}>
+				{props.buttonText}
+			</button>
+		</div>
+	);
+};
+
+export default Banner;
