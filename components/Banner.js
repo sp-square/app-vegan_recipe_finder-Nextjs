45b3146f9fdef61b8cc8981294438b2ac0ab754a
Sandrine Poissonnet <spoisson@gmail.com>
2022-04-02 09:50:45 -0700

    apply fonts

--- components/Banner.js
+++ components/Banner.js
@@ -8,9 +8,11 @@ const Banner = (props) => {
 				<span className={styles.title2}>Verte</span>
 			</h1>
 			<p className={styles.subtitle}>Discover plant-based recipes!</p>
-			<button className={styles.button} onClick={props.handleOnClick}>
-				{props.buttonText}
-			</button>
+			<div className={styles.buttonWrapper}>
+				<button className={styles.button} onClick={props.handleOnClick}>
+					{props.buttonText}
+				</button>
+			</div>
 		</div>
 	);
 };
